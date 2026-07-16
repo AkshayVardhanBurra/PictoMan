58d4908ea1daa913d23d5a7a72b2a4b0220e56a2
Akshay Vardhan Burra <142847672+AkshayVardhanBurra@users.noreply.github.com>
2026-07-16 12:39:49 -0700

    changed secrets location

--- frontend/src/pages/GamePageLogic.js
+++ frontend/src/pages/GamePageLogic.js
@@ -1,4 +1,4 @@
-import { LLMKEY } from "../../../secrets";
+import { LLMKEY } from "../../secrets";
 import { redirectToLogin } from "../Home";
 import { currentGuesses, resetGuesses } from "./GamePage";
 
